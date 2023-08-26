8962d9bd6d9926d83c10490c6a5f9b6b9bee115a
Vinicius Modesto <vmcmodesto@gmail.com>
2023-08-26 20:29:12 -0300

    Initial Commit

---
+++ MemorizeGame/MemorizeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeGameApp.swift
+//  MemorizeGame
+//
+//  Created by Vinicius Modesto on 26/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
