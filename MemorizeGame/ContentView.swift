e3d21bc78e11b7b1bf8c6e066bd1f47f9f55c3c6
Vinicius Modesto <vmcmodesto@gmail.com>
2023-08-26 20:38:06 -0300

    first commit

--- MemorizeGame/ContentView.swift
+++ MemorizeGame/ContentView.swift
@@ -1,26 +1,62 @@
 //
 //  ContentView.swift
-//  MemorizeGame
+//  Memorize
 //
-//  Created by Vinicius Modesto on 26/08/23.
+//  Created by Vinicius Modesto on 22/08/23.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            CardView(isFacedUp: true)
+            CardView(isFacedUp: false)
+            CardView(isFacedUp: false)
+            CardView(isFacedUp: true)
         }
-        .padding()
+        .padding(.horizontal)
+        .foregroundColor(.red)
     }
 }
+
+struct CardView : View {
+    @State var isFacedUp : Bool
+    
+    var body: some View {
+        ZStack {
+           let shape = RoundedRectangle(cornerRadius:20)
+            if isFacedUp {
+                shape
+                .fill()
+                .foregroundColor(.white)
+                shape
+                .stroke(lineWidth:3)
+                Text("☺️")
+                    .font(.largeTitle)
+            }
+            else {
+                RoundedRectangle(cornerRadius:20)
+                    .fill()
+            }
+             
+        }
+        .onTapGesture{
+            isFacedUp = !isFacedUp;
+        }
+        
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
+        ContentView()
+            .preferredColorScheme(.dark)
     }
+ 
 }
+
